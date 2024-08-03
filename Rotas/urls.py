122a6c55147fcf122f8755520f4aab5b0c5ada60
Pedro Henrique <phsl12.rf@gmail.com>
2024-08-03 13:25:57 -0300

    pagina_pedro

--- Rotas/urls.py
+++ Rotas/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pedro_jackson.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pedro/', pedro, name='pedro'),
 ]
--- pedro_jackson/views.py
+++ pedro_jackson/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def pedro (request):
+    return render(request, 'pedro_jackson/pedro.html')
